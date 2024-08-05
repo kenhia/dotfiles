27d58fecf5b1b0fde7cdfd9af68b06695459ec59
Ken Hiatt <ken.hiatt@gmail.com>
2024-08-05 06:22:04 -0700

    rrcode allow specifying dir

--- fish/.config/fish/functions/rrcode.fish
+++ fish/.config/fish/functions/rrcode.fish
@@ -1,5 +1,9 @@
-function rrcode --description 'call krrr via curl'
+function rrcode --description 'call krrr via curl' --argument-names 'dirname'
+    set -l local_dir $PWD
+    if test -n "$dirname"
+        set local_dir $(realpath $dirname)
+    end
     set -l host (string split -f 1 ' ' $SSH_CLIENT)
-    echo "Calling krrr on $host with USER=$USER and PWD=$PWD"
-    curl --request GET "http://$host:42271/rcode?client=karch9&user=$USER&path=$PWD"
+    echo "Calling krrr on $host with user=$USER and path=$local_dir"
+    curl --request GET "http://$host:42271/rcode?client=karch9&user=$USER&path=$local_dir"
 end
